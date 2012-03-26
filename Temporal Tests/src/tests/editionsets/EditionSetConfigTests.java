42074ee5f6cb96f870e20c03b21e70fab5ecc89f
dclarke <douglas.clarke@oracle.com>
2012-03-26 10:23:17 -0400

    New Temporal Tests project - uses specific JDK with agent configured - 96/96 tests pass

---
+++ Temporal Tests/src/tests/editionsets/EditionSetConfigTests.java
@@ -0,0 +1,130 @@
+/*******************************************************************************
+ * Copyright (c) 2011-2012 Oracle. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse
+ * Public License v1.0 and Eclipse Distribution License v. 1.0 which accompanies
+ * this distribution. The Eclipse Public License is available at
+ * http://www.eclipse.org/legal/epl-v10.html and the Eclipse Distribution
+ * License is available at http://www.eclipse.org/org/documents/edl-v10.php.
+ * 
+ * Contributors: dclarke - Bug 361016: Future Versions Examples
+ ******************************************************************************/
+package tests.editionsets;
+
+import static example.PersonModelExample.T2;
+import static example.PersonModelExample.T6;
+import junit.framework.Assert;
+import model.Person;
+
+import org.eclipse.persistence.descriptors.ClassDescriptor;
+import org.eclipse.persistence.mappings.OneToManyMapping;
+import org.eclipse.persistence.mappings.VariableOneToOneMapping;
+import org.eclipse.persistence.sessions.server.Server;
+import org.junit.Test;
+
+import temporal.EditionSet;
+import temporal.TemporalEntityManager;
+import temporal.TemporalHelper;
+import tests.BaseTestCase;
+import example.PersonModelExample;
+
+/**
+ * Tests verifying the {@link EditionSet} capabilities.
+ * 
+ * @author dclarke
+ * @since EclipseLink 2.3.1
+ */
+public class EditionSetConfigTests extends BaseTestCase {
+
+    private static PersonModelExample example = new PersonModelExample();
+
+    private Person getSample() {
+        return example.fullPerson;
+    }
+
+    @Test
+    public void verifyEditionSetMapping() {
+        TemporalEntityManager em = getEntityManager();
+
+        ClassDescriptor desc = em.unwrap(Server.class).getClassDescriptorForAlias("EditionSet");
+        Assert.assertNotNull(desc);
+
+        OneToManyMapping otmMapping = (OneToManyMapping) desc.getMappingForAttributeName("entries");
+        Assert.assertNotNull(otmMapping);
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @Test
+    public void verifyEditionSetEntryMapping() {
+        TemporalEntityManager em = getEntityManager();
+
+        ClassDescriptor desc = em.unwrap(Server.class).getClassDescriptorForAlias("EditionSetEntry");
+        Assert.assertNotNull(desc);
+
+        VariableOneToOneMapping votoMapping = (VariableOneToOneMapping) desc.getMappingForAttributeName("temporal");
+        Assert.assertNotNull(votoMapping);
+
+        Assert.assertEquals(8, votoMapping.getTypeIndicatorTranslation().size());
+
+        Assert.assertTrue(votoMapping.getTypeIndicatorTranslation().containsKey("Person"));
+        Assert.assertTrue(TemporalHelper.isEditionClass((Class) votoMapping.getTypeIndicatorTranslation().get("Person")));
+
+        Assert.assertTrue(votoMapping.getTypeIndicatorTranslation().containsKey("Phone"));
+        Assert.assertTrue(TemporalHelper.isEditionClass((Class) votoMapping.getTypeIndicatorTranslation().get("Phone")));
+
+        Assert.assertTrue(votoMapping.getTypeIndicatorTranslation().containsKey("Address"));
+        Assert.assertTrue(TemporalHelper.isEditionClass((Class) votoMapping.getTypeIndicatorTranslation().get("Address")));
+
+        Assert.assertTrue(votoMapping.getTypeIndicatorTranslation().containsKey("PersonHobby"));
+        Assert.assertTrue(TemporalHelper.isTemporal((Class) votoMapping.getTypeIndicatorTranslation().get("PersonHobby"), false));
+    }
+
+    @Test
+    public void verifySetEffectiveInitialize() {
+        TemporalEntityManager em = getEntityManager();
+
+        em.setEffectiveTime(T6);
+        EditionSet es = em.getEditionSet();
+
+        Assert.assertNotNull(es);
+        Assert.assertEquals(T6, es.getEffective());
+    }
+
+    @Test
+    public void verifyEditionSetPerEffective() {
+        TemporalEntityManager em = getEntityManager();
+
+        em.setEffectiveTime(T2);
+        EditionSet esT2 = em.getEditionSet();
+        Assert.assertNotNull(esT2);
+        Assert.assertEquals(T2, esT2.getEffective());
+
+        em.setEffectiveTime(T6);
+        EditionSet esT6 = em.getEditionSet();
+        Assert.assertNotNull(esT6);
+        Assert.assertEquals(T6, esT6.getEffective());
+        
+        Assert.assertNotSame(esT2, esT6);
+    }
+
+    @Test
+    public void verifyNotInitialized() {
+        TemporalEntityManager em = getEntityManager();
+        Assert.assertFalse(em.hasEditionSet());
+
+        em.setEffectiveTime(T6);
+
+        Assert.assertFalse(em.hasEditionSet());
+    }
+
+    /**
+     * Populate initial sample entity
+     */
+    @Override
+    public void populate(TemporalEntityManager em) {
+        System.out.println("\nEditionSetTests.populate:START");
+        example.populateHobbies(em);
+        em.persist(getSample());
+        System.out.println("\nEditionSetTests.populate::DONE");
+    }
+
+}
---
+++ Temporal Tests/src/tests/internal/TemporalEntityManagerTests.java
@@ -0,0 +1,224 @@
+/*******************************************************************************
+ * Copyright (c) 2011-2012 Oracle. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse
+ * Public License v1.0 and Eclipse Distribution License v. 1.0 which accompanies
+ * this distribution. The Eclipse Public License is available at
+ * http://www.eclipse.org/legal/epl-v10.html and the Eclipse Distribution
+ * License is available at http://www.eclipse.org/org/documents/edl-v10.php.
+ * 
+ * Contributors: dclarke - Bug 361016: Future Versions Examples
+ ******************************************************************************/
+package tests.internal;
+
+import static example.PersonModelExample.T1;
+import static example.PersonModelExample.T3;
+import static example.PersonModelExample.T4;
+import static example.PersonModelExample.T5;
+import static example.PersonModelExample.T6;
+import static example.PersonModelExample.T7;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+import static temporal.Effectivity.BOT;
+import static temporal.Effectivity.EOT;
+
+import javax.persistence.EntityManager;
+
+import junit.framework.Assert;
+import model.Person;
+import model.PersonHobby;
+
+import org.eclipse.persistence.internal.sessions.RepeatableWriteUnitOfWork;
+import org.eclipse.persistence.sessions.DatabaseSession;
+import org.eclipse.persistence.sessions.UnitOfWork;
+import org.eclipse.persistence.sessions.server.ClientSession;
+import org.eclipse.persistence.sessions.server.Server;
+import org.junit.Test;
+
+import temporal.EditionSet;
+import temporal.TemporalEntityManager;
+import tests.BaseTestCase;
+
+/**
+ * Verify the {@link TemporalEntityManager} API
+ * 
+ * @author dclarke
+ * @since EclipseLink 2.3.1
+ */
+public class TemporalEntityManagerTests extends BaseTestCase {
+
+    private void verifySetStartTime(TemporalEntityManager em, Long value) {
+        assertTrue(em.hasEffectiveTime());
+        assertNotNull(em.getEffectiveTime());
+        assertEquals(value, em.getEffectiveTime());
+
+        RepeatableWriteUnitOfWork uow = em.unwrap(RepeatableWriteUnitOfWork.class);
+        assertNotNull(uow);
+
+        ClientSession clientSession = (ClientSession) uow.getParent();
+
+        DatabaseSession session = em.unwrap(DatabaseSession.class);
+        assertNotSame(clientSession, session);
+
+        Server serverSession = em.unwrap(Server.class);
+        assertNotSame(clientSession, serverSession);
+        assertSame(session, serverSession);
+    }
+
+    @Test
+    public void verifyGetInstance() {
+        TemporalEntityManager em1 = getEntityManager();
+
+        Assert.assertTrue(em1.getProperties().containsKey(TemporalEntityManager.TEMPORAL_EM_PROPERTY));
+        Assert.assertFalse(em1.getProperties().containsKey(TemporalEntityManager.EFF_TS_PROPERTY));
+        assertFalse(em1.hasEffectiveTime());
+        assertNull(em1.getEffectiveTime());
+
+        EntityManager wrappedEm = em1.unwrap(EntityManager.class);
+        Assert.assertNotSame(em1, wrappedEm);
+
+        TemporalEntityManager em2 = TemporalEntityManager.getInstance(wrappedEm);
+        Assert.assertSame(em1, em2);
+
+        TemporalEntityManager em3 = TemporalEntityManager.getInstance(wrappedEm.unwrap(UnitOfWork.class));
+        Assert.assertSame(em1, em3);
+
+        TemporalEntityManager em4 = TemporalEntityManager.getInstance(em1);
+        Assert.assertSame(em1, em4);
+    }
+
+    @Test
+    public void verifySetStartTime() {
+        TemporalEntityManager em = getEntityManager();
+
+        assertFalse(em.hasEffectiveTime());
+        assertNull(em.getEffectiveTime());
+
+        em.setEffectiveTime(T1);
+
+        verifySetStartTime(em, T1);
+    }
+
+    @Test
+    public void verifyClearStartTime() {
+        TemporalEntityManager em = getEntityManager();
+        assertFalse(em.hasEffectiveTime());
+        assertNull(em.getEffectiveTime());
+
+        em.setEffectiveTime(T3);
+
+        verifySetStartTime(em, T3);
+
+        em.clearEffectiveTime();
+
+        assertFalse(em.hasEffectiveTime());
+        assertNull(em.getEffectiveTime());
+    }
+
+    @Test
+    public void verifyConcurrentSetStartTime() {
+        TemporalEntityManager em1 = getEntityManager();
+        TemporalEntityManager em2 = TemporalEntityManager.getInstance(getEMF().createEntityManager());
+
+        assertNotSame(em1, em2);
+
+        em1.setEffectiveTime(T4);
+
+        em2.setEffectiveTime(T5);
+
+        verifySetStartTime(em2, T5);
+        verifySetStartTime(em1, T4);
+    }
+
+    @Test
+    public void verifyConcurrentClearStartTime() {
+        TemporalEntityManager em1 = getEntityManager();
+        TemporalEntityManager em2 = TemporalEntityManager.getInstance(getEMF().createEntityManager());
+
+        assertNotSame(em1, em2);
+
+        em1.setEffectiveTime(T6);
+        em2.setEffectiveTime(T7);
+
+        verifySetStartTime(em2, T7);
+        verifySetStartTime(em1, T6);
+
+        em1.clearEffectiveTime();
+        verifySetStartTime(em2, T7);
+        assertFalse(em1.hasEffectiveTime());
+        assertNull(em1.getEffectiveTime());
+
+        em2.clearEffectiveTime();
+        assertFalse(em2.hasEffectiveTime());
+        assertNull(em2.getEffectiveTime());
+    }
+
+    @Test
+    public void verifyCurrentCreateTemporal() {
+        TemporalEntityManager em = getEntityManager();
+        em.getTransaction().begin();
+
+        PersonHobby ph = em.newTemporal(PersonHobby.class);
+
+        Assert.assertNotNull(ph);
+        Assert.assertNotNull(ph.getEffectivity());
+        Assert.assertEquals(BOT, ph.getEffectivity().getStart());
+        Assert.assertEquals(EOT, ph.getEffectivity().getEnd());
+    }
+
+    @Test
+    public void verifyFutureCreateTemporal() {
+        TemporalEntityManager em = getEntityManager();
+        em.setEffectiveTime(T3);
+        em.getTransaction().begin();
+
+        PersonHobby ph = em.newTemporal(PersonHobby.class);
+
+        Assert.assertNotNull(ph);
+        Assert.assertNotNull(ph.getEffectivity());
+        Assert.assertEquals(T3, ph.getEffectivity().getStart());
+        Assert.assertEquals(EOT, ph.getEffectivity().getEnd());
+    }
+
+    @Test
+    public void verifyChangeEffectiveWithPendingChanges() {
+        TemporalEntityManager em = getEntityManager();
+        em.setEffectiveTime(T3);
+        EditionSet esT3 = em.getEditionSet();
+        
+        em.getTransaction().begin();
+
+        Person newPersonAtT3 = em.newEntity(Person.class);
+        Assert.assertNotNull(newPersonAtT3);
+        Assert.assertEquals(T3, newPersonAtT3.getEffectivity().getStart());
+        Assert.assertTrue(esT3.hasChanges());
+
+        em.setEffectiveTime(T4);
+        EditionSet esT4 = em.getEditionSet();
+        
+        Assert.assertFalse(esT4.hasChanges());
+        Assert.assertTrue(esT3.hasChanges());
+        
+        em.flush();
+    }
+
+    @Test
+    public void verifyRollbackWithPendingChanges() {
+        TemporalEntityManager em = getEntityManager();
+        em.setEffectiveTime(T3);
+        em.getTransaction().begin();
+
+        Person newPersonAtT3 = em.newEntity(Person.class);
+        Assert.assertNotNull(newPersonAtT3);
+        Assert.assertEquals(T3, newPersonAtT3.getEffectivity().getStart());
+        Assert.assertTrue(em.hasEditionSet());
+        Assert.assertTrue(em.getEditionSet().hasChanges());
+
+        // TODO : ?
+        em.getTransaction().rollback();
+    }
+}
